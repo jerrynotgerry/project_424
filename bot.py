ff25a5fafb66d645f7ccc9412c6cbf0afe630379
jerrynotgerry <mazurekj3@gmail.com>
2020-11-15 01:48:54 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,12 @@ async def on_ready():
 @bot.command(name='hello', help='Responds with hello world')
 async def hello(ctx):
     await ctx.send('Hello World!')
-    
-    
+
+@bot.command(name='connect', help='Connects to your currently occupied voice channel')
+async def connect(ctx):
+    if ctx.author.channel == None:
+        await ctx.send('You are not in any voice channel.')
+        return
+    await ctx.author.channel.connect()
 
 bot.run(TOKEN)
